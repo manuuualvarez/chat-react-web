c6f152e00c55302f7953ea80b04591ab48d28b96
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-03-09 19:58:07 -0300

    listen the event with the status of the users

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,8 +1,11 @@
 
-import React, { useContext, useEffect } from 'react';
-import { createContext } from 'react';
+import React, { useContext, useEffect, createContext } from 'react';
 import { useSocket } from '../hooks/useSocket'
+import { types } from '../types/types';
+
+
 import { AuthContext } from '../auth/AuthContext';
+import { ChatContext } from './chat/ChatContext';
 
 export const SocketContext = createContext();
 
@@ -11,6 +14,7 @@ export const SocketProvider = ({ children }) => {
 
     const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080');
     const { auth } = useContext(AuthContext);
+    const { dispatch } = useContext( ChatContext)
 
     // Auth Login Effect
     useEffect(() => {
@@ -27,7 +31,10 @@ export const SocketProvider = ({ children }) => {
     // Sockets Effect: Listen changes in online users
     useEffect(() => {
         socket?.on('active-users', (users) => {
-            console.log(users);
+            dispatch({
+                type: types.usersLoaded,
+                payload: users
+            });
         });
     }, [socket]);
     
