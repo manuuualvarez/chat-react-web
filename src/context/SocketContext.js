0428836f4e64c53553defb76a24c36f629e77a3f
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-03-09 19:46:21 -0300

    get the users from the database and socket

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -12,17 +12,24 @@ export const SocketProvider = ({ children }) => {
     const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080');
     const { auth } = useContext(AuthContext);
 
+    // Auth Login Effect
     useEffect(() => {
         if(auth.logged){
             connectSocket();
         }
     }, [auth, connectSocket]);
-
+    // Auth Logout Effect
     useEffect(() => {
         if(!auth.logged){
             disconnectSocket();
         }
     }, [auth, disconnectSocket]);
+    // Sockets Effect: Listen changes in online users
+    useEffect(() => {
+        socket?.on('active-users', (users) => {
+            console.log(users);
+        });
+    }, [socket]);
     
     return (
         <SocketContext.Provider value={{ socket, online }}>
